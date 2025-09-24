80ea9aed744b707fbf26635b8ad4f439e96484ee
Festus <festusyuma@gmail.com>
2025-09-24 21:37:11 +0100

    load saved vouchers from db

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use chrono::offset;
+use dotenvy::dotenv;
+use postgres::{Client, NoTls};
 use rand::Rng;
 use rand::distr::{Alphanumeric, SampleString};
 use std::collections::HashSet;
@@ -26,17 +28,59 @@ fn validate_voucher_pin(pin: &str) -> bool {
 }
 
 fn main() {
+    dotenv().ok();
+
+    let args: Vec<String> = env::args().collect();
+    let promo_id: &String = &args[1].parse().expect("promo id is required");
+    let no_of_vouchers: &i32 = &args[2].clone().parse().expect("enter number of vouchers");
+    let batch_size: &i32 = &args[3].clone().parse().expect("enter batch size");
+    let output_file: &str = &args[4];
+
     let mut rng = rand::rng();
 
     let mut existing_pins = HashSet::new();
     let mut existing_serials = HashSet::new();
     let mut vouchers_generated = 0;
 
-    let args: Vec<String> = env::args().collect();
-    let no_of_vouchers: &i32 = &args[1].clone().parse().expect("enter number of vouchers");
-    let output_file = &args[2];
+    // Access an environment variable
+    let db_url_str = env::var("DATABASE_URL").expect("DATABASE_URL must be set in .env file");
 
-    let mut file = fs::File::create(output_file).unwrap();
+    let mut db_url = url::Url::parse(&db_url_str).expect("Failed to parse url");
+    db_url.set_query(None);
+
+    let mut db_client = Client::connect(&db_url.to_string(), NoTls).unwrap();
+    let mut last_pin: String = String::from("");
+    let mut vouchers_loaded = 0;
+
+    let query = db_client
+        .prepare("SELECT pin, serial, promo_id FROM \"Voucher\" WHERE promo_id = $1 and pin > $2 ORDER BY pin LIMIT 100000")
+        .expect("Error preparing query");
+
+    loop {
+        let items = db_client.query(&query, &[promo_id, &last_pin]).unwrap();
+
+        vouchers_loaded += items.len();
+        last_pin = String::from("");
+
+        for row in items {
+            let pin: String = row.get(0);
+            let serial: String = row.get(1);
+
+            existing_pins.insert(pin.to_uppercase());
+            existing_serials.insert(serial.to_uppercase());
+            last_pin = pin;
+        }
+
+        if last_pin == "" {
+            break;
+        }
+
+        println!("Loaded {vouchers_loaded} from DB")
+    }
+
+    let mut current_batch = 1;
+
+    let mut file = fs::File::create(format!("{output_file}-{current_batch}.csv")).unwrap();
     file.write("pin, serial\n".as_bytes()).unwrap();
 
     loop {
@@ -48,14 +92,20 @@ fn main() {
                 generated = String::new();
 
                 loop {
-                    let generated_char = Alphanumeric.sample_string(&mut rng, 1).to_uppercase();
-                    let generated_char = generated_char.as_str();
+                    let generated_char = Alphanumeric
+                        .sample_string(&mut rng, 1)
+                        .to_uppercase()
+                        .chars()
+                        .last()
+                        .expect("unable to generate character");
 
-                    if generated.contains(generated_char) {
-                        continue;
+                    if generated.len() > 0 {
+                        if generated.chars().last().unwrap() == generated_char {
+                            continue;
+                        }
                     }
 
-                    generated.push_str(generated_char);
+                    generated.push(generated_char);
 
                     if generated.len() == 5 {
                         break;
@@ -81,16 +131,21 @@ fn main() {
         existing_pins.insert(pin);
         existing_serials.insert(serial);
 
-        if &vouchers_generated % 1_000_000 == 0 {
-            println!(
-                "Generated {} vouchers at {}",
-                vouchers_generated,
-                offset::Local::now()
-            )
-        }
-
         if &vouchers_generated == no_of_vouchers {
             break;
+        }
+
+        if &vouchers_generated % batch_size == 0 {
+            current_batch += 1;
+
+            println!(
+                "Batch complete: Generated {} vouchers at {}",
+                vouchers_generated,
+                offset::Local::now()
+            );
+
+            file = fs::File::create(format!("{output_file}-{current_batch}.csv")).unwrap();
+            file.write("pin, serial\n".as_bytes()).unwrap();
         }
     }
 }
