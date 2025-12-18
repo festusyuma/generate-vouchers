c9015557103e1eaf573a866fb4311df8538f3eb3
Festus <festusyuma@gmail.com>
2025-12-18 13:42:02 +0100

    generate vouchers in batches

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use chrono::offset;
 use rand::Rng;
-use rand::distr::{Alphanumeric, SampleString};
 use std::collections::HashSet;
 use std::{env, fs, io::Write};
 
-fn validate_voucher_pin(pin: &str) -> bool {
+fn _validate_voucher_pin(pin: &str) -> bool {
     let mut has_char = false;
     let mut has_digit = false;
 
@@ -32,33 +31,36 @@ fn main() {
     let mut existing_serials = HashSet::new();
     let mut vouchers_generated = 0;
 
+    let mut current_batch = 1;
     let args: Vec<String> = env::args().collect();
     let no_of_vouchers: &i32 = &args[1].clone().parse().expect("enter number of vouchers");
-    let output_file = &args[2];
+    let batch_size: &i32 = &args[2].clone().parse().expect("enter batch size");
+    let output_file = &args[3];
 
-    let mut file = fs::File::create(output_file).unwrap();
+    let mut file = fs::File::create(format!("{output_file}-{current_batch}.csv")).unwrap();
     file.write("pin, serial\n".as_bytes()).unwrap();
 
     loop {
-        if &vouchers_generated == no_of_vouchers {
-            break;
-        }
-
-        if &vouchers_generated % 1_000_000 == 0 {
-            println!(
-                "Generated {} vouchers at {}",
-                vouchers_generated,
-                offset::Local::now()
-            )
-        }
-
         let serial: String = (0..20).map(|_| rng.random_range('0'..'9')).collect();
         let pin = {
             let mut generated;
+            let mut rng = rand::rng();
+
+            generated = String::new();
 
             loop {
-                generated = Alphanumeric.sample_string(&mut rng, 9).to_uppercase();
-                if validate_voucher_pin(&generated) {
+                let generated_char: u8 = rng.random_range(0..10);
+                let generated_char = (b'0' + generated_char) as char;
+
+                if let Some(last_char) = generated.chars().last() {
+                    if last_char == generated_char {
+                        continue;
+                    }
+                }
+
+                generated.push(generated_char);
+
+                if generated.len() == 9 {
                     break;
                 }
             }
@@ -76,5 +78,22 @@ fn main() {
         vouchers_generated += 1;
         existing_pins.insert(pin);
         existing_serials.insert(serial);
+
+        if &vouchers_generated == no_of_vouchers {
+            break;
+        }
+
+        if &vouchers_generated % batch_size == 0 {
+            current_batch += 1;
+
+            println!(
+                "Batch complete: Generated {} vouchers at {}",
+                vouchers_generated,
+                offset::Local::now()
+            );
+
+            file = fs::File::create(format!("{output_file}-{current_batch}.csv")).unwrap();
+            file.write("pin, serial\n".as_bytes()).unwrap();
+        }
     }
 }
